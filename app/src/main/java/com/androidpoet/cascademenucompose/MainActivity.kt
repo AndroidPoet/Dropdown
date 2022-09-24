f14152472e51740943fe32004a7bdc85637c53c8
AndroidPoet <poetdroid2@gmail.com>
2022-09-24 17:43:47 +0530

    Update dependencies

--- app/src/main/java/com/androidpoet/cascademenucompose/MainActivity.kt
+++ app/src/main/java/com/androidpoet/cascademenucompose/MainActivity.kt
@@ -1,4 +1,3 @@
-
 package com.androidpoet.cascademenucompose
 
 import android.os.Bundle
@@ -11,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -50,130 +50,131 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.receiveAsFlow
 
 class MainActivity : ComponentActivity() {
-  @OptIn(ExperimentalAnimationApi::class)
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      CascadeMenuComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background
-        ) {
-          DropDownMenuScreen()
+    @OptIn(ExperimentalAnimationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CascadeMenuComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DropDownMenuScreen()
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-  CascadeMenuComposeTheme {
-    DropDownMenuScreen()
-  }
+    CascadeMenuComposeTheme {
+        DropDownMenuScreen()
+    }
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 @ExperimentalAnimationApi
 @Composable
 fun DropDownMenuScreen() {
-  val snackbarHostState = remember { SnackbarHostState() }
-  val channel = remember { Channel<String>(Channel.CONFLATED) }
-  val (isOpen, setIsOpen) = remember { mutableStateOf(false) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val channel = remember { Channel<String>(Channel.CONFLATED) }
+    val (isOpen, setIsOpen) = remember { mutableStateOf(false) }
 
-  LaunchedEffect(key1 = channel) {
-    channel.receiveAsFlow().collect {
-      snackbarHostState.showSnackbar(it)
-    }
-  }
-  Scaffold(
-    modifier = Modifier.fillMaxSize(),
-    backgroundColor = Color.Transparent,
-    scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState)
-  ) {
-    Column(
-      modifier = Modifier
-        .fillMaxSize(),
-      verticalArrangement = Arrangement.Top,
-      horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-      Spacer(modifier = Modifier.height(48.dp))
-      Box(contentAlignment = Alignment.TopEnd) {
-        Menu(isOpen = isOpen, setIsOpen = setIsOpen, itemSelected = {
-          channel.trySend(it)
-          setIsOpen(false)
-        })
-        IconButton(
-          onClick = { setIsOpen(true) }
-        ) {
-          Icon(
-            imageVector = Icons.TwoTone.MoreVert,
-            contentDescription = null
-          )
+    LaunchedEffect(key1 = channel) {
+        channel.receiveAsFlow().collect {
+            snackbarHostState.showSnackbar(it)
         }
-      }
     }
-  }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        backgroundColor = Color.Transparent,
+        scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState)
+    ) { padding ->
+        Column(
+            modifier = Modifier
+              .padding(padding)
+              .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(48.dp))
+            Box(contentAlignment = Alignment.TopEnd) {
+                Menu(isOpen = isOpen, setIsOpen = setIsOpen, itemSelected = {
+                    channel.trySend(it)
+                    setIsOpen(false)
+                })
+                IconButton(
+                    onClick = { setIsOpen(true) }
+                ) {
+                    Icon(
+                        imageVector = Icons.TwoTone.MoreVert,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
 }
 
 // Dropdown menu
 @ExperimentalAnimationApi
 @Composable
 fun Menu(isOpen: Boolean = false, setIsOpen: (Boolean) -> Unit, itemSelected: (String) -> Unit) {
-  val menu = getMenu()
-  Dropdown(
+    val menu = getMenu()
+    Dropdown(
 
-    isOpen = isOpen,
-    menu = menu,
-    colors = dropDownMenuColors(Teal200, Color.Black),
-    onItemSelected = itemSelected,
-    onDismiss = { setIsOpen(false) },
-    offset = DpOffset(8.dp, 0.dp),
-    enter = EnterAnimation.ExpandVertically,
-    exit = ExitAnimation.ShrinkVertically,
-    easing = Easing.FastOutLinearInEasing,
-    enterDuration = 400,
-    exitDuration = 400
+        isOpen = isOpen,
+        menu = menu,
+        colors = dropDownMenuColors(Teal200, Color.Black),
+        onItemSelected = itemSelected,
+        onDismiss = { setIsOpen(false) },
+        offset = DpOffset(8.dp, 0.dp),
+        enter = EnterAnimation.ExpandVertically,
+        exit = ExitAnimation.ShrinkVertically,
+        easing = Easing.FastOutLinearInEasing,
+        enterDuration = 400,
+        exitDuration = 400
 
-  )
+    )
 }
 
 // menu builder
 fun getMenu(): MenuItem<String> {
-  val menu = dropDownMenu<String> {
-    item("about", "About") {
-      icon(Icons.TwoTone.Language)
+    val menu = dropDownMenu<String> {
+        item("about", "About") {
+            icon(Icons.TwoTone.Language)
+        }
+        item("copy", "Copy") {
+            icon(Icons.TwoTone.FileCopy)
+        }
+        item("share", "Share") {
+            icon(Icons.TwoTone.Share)
+            item("to_clipboard", "To clipboard") {
+                item("pdf", "PDF")
+                item("epub", "EPUB")
+                item("web_page", "Web page")
+                item("microsoft_word", "Microsoft word")
+            }
+            item("as_a_file", "As a file") {
+                item("pdf", "PDF")
+                item("epub", "EPUB")
+                item("web_page", "Web page")
+                item("microsoft_word", "Microsoft word")
+            }
+        }
+        item("remove", "Remove") {
+            icon(Icons.TwoTone.DeleteSweep)
+            item("yep", "Yep") {
+                icon(Icons.TwoTone.Done)
+            }
+            item("go_back", "Go back") {
+                icon(Icons.TwoTone.Close)
+            }
+        }
     }
-    item("copy", "Copy") {
-      icon(Icons.TwoTone.FileCopy)
-    }
-    item("share", "Share") {
-      icon(Icons.TwoTone.Share)
-      item("to_clipboard", "To clipboard") {
-        item("pdf", "PDF")
-        item("epub", "EPUB")
-        item("web_page", "Web page")
-        item("microsoft_word", "Microsoft word")
-      }
-      item("as_a_file", "As a file") {
-        item("pdf", "PDF")
-        item("epub", "EPUB")
-        item("web_page", "Web page")
-        item("microsoft_word", "Microsoft word")
-      }
-    }
-    item("remove", "Remove") {
-      icon(Icons.TwoTone.DeleteSweep)
-      item("yep", "Yep") {
-        icon(Icons.TwoTone.Done)
-      }
-      item("go_back", "Go back") {
-        icon(Icons.TwoTone.Close)
-      }
-    }
-  }
-  return menu
+    return menu
 }
