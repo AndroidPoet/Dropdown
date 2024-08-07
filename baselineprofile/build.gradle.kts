ca81d93f555d8a9700df49e41b9815d5185b51c7
Ranbir Singh <poetdroid2@gmail.com>
2024-08-07 11:56:18 +0530

    publish to maven fixed

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -31,19 +31,26 @@ android {
   // This code creates the gradle managed device used to generate baseline profiles.
   // To use GMD please invoke generation through the command line:
   // ./gradlew :composeApp:generateBaselineProfile
+
   testOptions.managedDevices.devices {
-    create<ManagedVirtualDevice>("pixel6Api34") {
+    maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel6api31").apply {
       device = "Pixel 6"
-      apiLevel = 34
-      systemImageSource = "google"
+      apiLevel = 31
+      systemImageSource = "aosp"
     }
   }
 }
 
-// This is the configuration block for the Baseline Profile plugin.
-// You can specify to run the generators on a managed devices or connected devices.
+// This is the plugin configuration. Everything is optional. Defaults are in the
+// comments. In this example, you use the GMD added earlier and disable connected devices.
 baselineProfile {
-  managedDevices += "pixel6Api34"
+
+  // This specifies the managed devices to use that you run the tests on. The default
+  // is none.
+  managedDevices += "pixel6api31"
+
+  // This enables using connected devices to generate profiles. The default is true.
+  // When using connected devices, they must be rooted or API 33 and higher.
   useConnectedDevices = false
 }
 
